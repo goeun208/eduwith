88113254e691714ec46c89a76d747489fcce00ad
chaeli <s.kcl5243@gmail.com>
2022-03-06 21:53:05 +0900

    3/6

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Routes } from "react-router-dom";
+import Main from "./routes/Main";
+import Login from "./components/Login";
+import Volunteer from './routes/Volunteer';
+import { createGlobalStyle } from 'styled-components';
+import { useState } from 'react';
+
+
+const GlobalStyle = createGlobalStyle
+  `body{
+        background: #e9ecef;
+      }`;
+
 
 function App() {
+
   return (
-    <div className="App">
-      Hello
-    </div>
+    <Main/>
+    /*<Router>
+      <Routes>
+        <Route path="/" element={<Main/>}>
+        </Route>
+        <Route path="/Volunteer" element={<Volunteer/>}>
+        </Route>
+      </Routes>
+    </Router>*/
   );
 }
 
 export default App;
+{/* <Main>
+      <div onClick={onLoginToggle}>로그인</div>
+      {loginToggle && <Login onLoginToggle={onLoginToggle} />}
+    </Main> */}
